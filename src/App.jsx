faefeed80ea758ebf1c713f6ea95da8bdaffaf88
Dino DiGiulio <ddigiulio@Coupland.local>
2017-01-19 15:19:25 -0500

    Added style basic framework and EndpointEntry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
-
+import {EndpointEntry} from './endpointEntry/';
 
 class App extends Component {
+
+	componentWillMount() {
+		let endpoint = '';
+		this.setState({endpoint});
+	}
+
+	changeEndpoint = (endpoint) => {
+		console.log('endpoint changed', endpoint);
+		this.setState({endpoint});
+	}
+
 	render() {
+		let {changeEndpoint} = this;
 		return (
 			<div className="App">
+				<EndpointEntry changeEndpoint={changeEndpoint}/>
 				<p className="App-intro">
 					To get started, edit
 					<code>src/App.js</code>
---
+++ src/endpointEntry/index.jsx
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import FontAwesome from 'react-fontawesome';
+import './index.less';
+
+export class EndpointEntry extends Component {
+
+	componentWillMount(){
+		let show = false;
+		let endpoint = '';
+		this.setState({show, endpoint});
+	}
+
+	toggleShow = () => {
+		let {show} = Object.assign(this.state);
+		show = !show;
+		this.setState({show});
+	}
+
+	onChange = (event) => {
+		let {value: endpoint} = event.target;
+		this.setState({endpoint});
+	}
+
+	getOnSubmit = (changeEndpoint) => {
+		return (event) => {
+			event.preventDefault();
+			let {endpoint} = this.state;
+			changeEndpoint(endpoint);
+			endpoint = '';
+			let show = false;
+			this.setState({endpoint, show});
+		};
+	}
+
+	render() {
+		let {toggleShow, state, props, getOnSubmit, onChange} = this;
+		let {changeEndpoint} = props;
+		let {show, endpoint} = state;
+		let onSubmit = getOnSubmit(changeEndpoint);
+		return (
+			<div className={show ? 'endpoint-entry show' : 'endpoint-entry'}>
+				<div className="show-hide-toggle" onClick={toggleShow}>
+					<FontAwesome name="code"></FontAwesome>
+				</div>
+				<form onSubmit={onSubmit}>
+					<label>Enter endpoint</label>
+					<input type="text" name="endpoint" value={endpoint} onChange={onChange} />
+				</form>
+			</div>
+		);
+	}
+}
